458af46795ecc6a75f4095756eb4f41f9aac789b
abdel <abdotha5@gmail.com>
2025-05-11 03:48:50 +0300

    update

--- lib/view/auth/login_screen/login_screen.dart
+++ lib/view/auth/login_screen/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:bookstore_app/core/router/router_names.dart';
 import 'package:bookstore_app/view_model/auth_view_model/auth_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:bookstore_app/core/common/widgets/custom_button.dart';
 import 'package:bookstore_app/core/constants/app_colors.dart';
